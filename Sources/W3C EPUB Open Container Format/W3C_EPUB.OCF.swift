49dbd75cb0eafa9c6d9a9a85d0f35119905a966a
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 20:06:18 +0200

    admin wip

--- Sources/W3C EPUB Open Container Format/W3C_EPUB.OCF.swift
+++ Sources/W3C EPUB Open Container Format/W3C_EPUB.OCF.swift
@@ -1,7 +1,7 @@
 // W3C EPUB 3.3 — Open Container Format (OCF)
 // https://www.w3.org/TR/epub-33/#sec-ocf
 
-public import W3C_EPUB_Package_Document
+import W3C_EPUB_Package_Document
 public import W3C_EPUB_Shared
 
 extension W3C_EPUB {
--- Sources/W3C EPUB Package Document/W3C_EPUB.PackageDocument.swift
+++ Sources/W3C EPUB Package Document/W3C_EPUB.PackageDocument.swift
@@ -1,8 +1,8 @@
 // W3C EPUB 3.3 — Package Document
 // https://www.w3.org/TR/epub-33/#sec-package-doc
 
-public import BCP_47
-public import W3C_EPUB_Publications
+import BCP_47
+import W3C_EPUB_Publications
 public import W3C_EPUB_Shared
 
 extension W3C_EPUB {
